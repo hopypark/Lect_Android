b1a97cb81928db73a82f0c2904a8e56d7a525070
hopypark <hopypark@gmail.com>
2023-06-05 17:38:43 +0900

    Update MainActivity.java and MyAirplaneReceive.java

--- BroadcastReceiverDemo/app/src/main/java/com/example/broadcastreceiverdemo/MainActivity.java
+++ BroadcastReceiverDemo/app/src/main/java/com/example/broadcastreceiverdemo/MainActivity.java
@@ -6,18 +6,31 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver airplaneReceiver;
+    public static final String MY_ACTION_BROADCAST = "MY Broadcasting...";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MY_ACTION_BROADCAST);
+                sendBroadcast(intent); // 인텐트를 사용한 Broadcast 보내기
+            }
+        });
+        
         // 방송수신자 등록 - 동적 등록
         airplaneReceiver = new MyAirplaneReceiver();
         IntentFilter filter = new IntentFilter(); // 받고자하는 방송 내용에 대한 필터 설정
         filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED); // AIRPLANE 모드가 변경되는 것
+        filter.addAction(MY_ACTION_BROADCAST); // filter 항목으로 MY_ACTION_BROADCAST 추가
         registerReceiver(airplaneReceiver, filter); // 리시버에 등록
     }
 
--- BroadcastReceiverDemo/app/src/main/java/com/example/broadcastreceiverdemo/MyAirplaneReceiver.java
+++ BroadcastReceiverDemo/app/src/main/java/com/example/broadcastreceiverdemo/MyAirplaneReceiver.java
@@ -11,11 +11,16 @@ public class MyAirplaneReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         // Broadcast Receiver의 상태 정보를 intent로부터 받음, 받는 값이 없다면 default값 false를 사용한다.
-        boolean state = intent.getBooleanExtra("state", false);
-        if (state){
-            Log.d(TAG, "Action: 비행기 모드 설정");
-        } else {
-            Log.d(TAG, "Action: 비행기 모드 해제");
+        if (intent.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
+            // 비행기 모드에 대한 방송수신 처리
+            boolean state = intent.getBooleanExtra("state", false);
+            if (state) {
+                Log.d(TAG, "Action: 비행기 모드 설정");
+            } else {
+                Log.d(TAG, "Action: 비행기 모드 해제");
+            }
+        }else if (intent.getAction().equals(MainActivity.MY_ACTION_BROADCAST)){
+            Log.d(TAG, "사용자가 보낸 방송을 받음");
         }
     }
 }
