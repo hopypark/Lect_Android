bc654d755805d7c20b0e8a8cf701c9e29ac4a132
hopypark <hopypark@gmail.com>
2023-07-14 13:28:34 +0900

    Update MusicPlayerDemo

--- MusicPlayerDemo/app/src/main/java/com/example/musicplayerdemo/MainActivity.kt
+++ MusicPlayerDemo/app/src/main/java/com/example/musicplayerdemo/MainActivity.kt
@@ -1,33 +1,11 @@
 package com.example.musicplayerdemo
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.IBinder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bindService()
-    }
-
-    val mServiceConnection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            // 서비스 연결 성공 TODO("Not yet implemented")
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            // 서비스 연결 실패(종료) TODO("Not yet implemented")
-        }
-    }
-
-    private fun bindService(){
-        val intent = Intent(this, MusicPlayerService::class.java)
-        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE)
     }
 }
-
