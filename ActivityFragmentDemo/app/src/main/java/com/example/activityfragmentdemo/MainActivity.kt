a14ecd49806c0037b05c64bb9563c1715b014624
hopypark <hopypark@gmail.com>
2023-07-19 11:26:41 +0900

    Update MainActivity.kt

--- ActivityFragmentDemo/app/src/main/java/com/example/activityfragmentdemo/MainActivity.kt
+++ ActivityFragmentDemo/app/src/main/java/com/example/activityfragmentdemo/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.activityfragmentdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        settingButtons()
+    }
+
+    private fun settingButtons() {
+        val redButton = findViewById<Button>(R.id.btn_red_fragment)
+        val blueButton = findViewById<Button>(R.id.btn_blue_fragment)
+
+        redButton.setOnClickListener{
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frame_layout, RedFragment())
+            fragmentTransaction.commit()
+        }
+
+        blueButton.setOnClickListener{
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frame_layout, RedFragment())
+            fragmentTransaction.commit()
+        }
     }
 }
+
