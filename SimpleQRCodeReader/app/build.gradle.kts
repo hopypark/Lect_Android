ddd6fa1cc46610cc79dd0c298e744cfb4efe9c50
hopypark <hopypark@gmail.com>
2023-07-28 14:21:24 +0900

    Update build.gradle.kts

--- SimpleQRCodeReader/app/build.gradle.kts
+++ SimpleQRCodeReader/app/build.gradle.kts
@@ -33,10 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
@@ -44,4 +47,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // CameraX 코어 라이브러리
+    implementation("androidx.camera:camera-camera2:1.2.3")
+    // CameraX 생명 주기 관련 라이브러리
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
+    // CameraX 뷰 관련 라이브러리
+    implementation("androidx.camera:camera-view:1.2.3")
+    // ML kit 라이브러리
+    implementation("com.google.mlkit:barcode-scanning:17.1.0")
+
 }
