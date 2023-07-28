f3d7ae54b62b419e8e892c228733ad3a957175dd
hopypark <hopypark@gmail.com>
2023-07-28 14:57:56 +0900

    Update MainActivity.kt

--- SimpleQRCodeReader/app/src/main/java/com/example/simpleqrcodereader/MainActivity.kt
+++ SimpleQRCodeReader/app/src/main/java/com/example/simpleqrcodereader/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.simpleqrcodereader
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.simpleqrcodereader.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding   // 바인딩 변수 선언
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+
+    private val PERMISSIONS_REQUEST_CODE = 1
+    private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root // binding.root == activity_main레이아웃 파일의 root 엘리먼트(ConstraintLayout)
+
         setContentView(R.layout.activity_main)
+
+        if (!hasPermissions(this)){
+            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+        } else {
+            startCamera()
+        }
+    }
+
+    private fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //
+        if (requestCode == PERMISSIONS_REQUEST_CODE){
+            if(PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()){
+                Toast.makeText(this@MainActivity, "권한 요청이 승인되었습니다.", Toast.LENGTH_LONG).show()
+                startCamera()
+            } else{
+                Toast.makeText(this@MainActivity, "권한 요청이 거부되었습니다.", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+    }
+
+    fun startCamera(){
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = getPreview()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    fun getPreview():Preview{
+        val preview : Preview = Preview.Builder().build()    // Preview()객체 생성
+        preview.setSurfaceProvider(binding.barcodePreview.getSurfaceProvider())
+        return preview
     }
 }
