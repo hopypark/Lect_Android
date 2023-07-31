0eb9c6b83ec5a5d66e30c7a5773ecf10d055a7aa
hopypark <hopypark@gmail.com>
2023-07-31 14:12:05 +0900

    Update ResultActivity.kt

--- SimpleQRCodeReader/app/src/main/java/com/example/simpleqrcodereader/ResultActivity.kt
+++ SimpleQRCodeReader/app/src/main/java/com/example/simpleqrcodereader/ResultActivity.kt
@@ -2,10 +2,23 @@ package com.example.simpleqrcodereader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simpleqrcodereader.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val result = intent.getStringExtra("msg")?:"데이터가 존재하지 않습니다."
+
+        setUI(result)
+    }
+
+    private fun setUI(result: String) {
+        binding.tvContent.text = result
+        binding.btnGoBack.setOnClickListener { finish() }
     }
 }
