c579eb2e614326c3ab3da59b742f154280ced77e
hopypark <hopypark@gmail.com>
2023-08-02 08:33:02 +0900

    Update build.gradle.kts

--- ViewBindingSample/app/build.gradle.kts
+++ ViewBindingSample/app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.viewbindingsample"
     compileSdk = 33
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.viewbindingsample"
         minSdk = 26
