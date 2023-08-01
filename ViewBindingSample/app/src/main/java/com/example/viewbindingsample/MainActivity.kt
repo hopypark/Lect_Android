621152f709edad56ba06de3b7df07ebbb4e6a569
hopypark <hopypark@gmail.com>
2023-08-02 08:33:06 +0900

    Update MainActivity.kt

--- ViewBindingSample/app/src/main/java/com/example/viewbindingsample/MainActivity.kt
+++ ViewBindingSample/app/src/main/java/com/example/viewbindingsample/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.viewbindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mTextView = findViewById(R.id.tv_textView)
     }
 }
+
