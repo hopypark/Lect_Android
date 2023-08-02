2932268d03a6145c01a868a0cbda26ef0895a4ff
hopypark <hopypark@gmail.com>
2023-08-02 13:54:09 +0900

    Update MainActivity.kt

--- ViewBindingSample/app/src/main/java/com/example/viewbindingsample/MainActivity.kt
+++ ViewBindingSample/app/src/main/java/com/example/viewbindingsample/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.viewbindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import com.example.viewbindingsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater) // 바인딩 클래스의 객체 생성
+
+        val rootView = binding.root;    // 바인딩 객체의 루트뷰 참조(ConstraintLayout)
+        setContentView(rootView)        // 생성한 뷰 설정
+
+        binding.bindingButton.setOnClickListener {
+            // do something...
+        }
     }
 }
 
