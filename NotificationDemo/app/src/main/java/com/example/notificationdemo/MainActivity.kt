e3f05cdfb657bb76c30f76696e1c3eccc8621ba5
hopypark <hopypark@gmail.com>
2023-08-04 09:16:14 +0900

    Update MainActivity.kt

--- NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.kt
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.notificationdemo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        msg()
+    }
+
+    fun msg(){
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channelId = "one-channel"
+            val channelName = "My Channel One"
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            // 채널에 다양한 정보 설정
+            channel.description = "My Channel One Description"
+            channel.setShowBadge(true)
+            val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+            channel.setSound(uri, audioAttributes)
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(100,200,100,200)
+            // 채널을 NotificationManager에 등록
+            manager.createNotificationChannel(channel)
+            // 채널을 이용해 빌더 생성
+            builder = NotificationCompat.Builder(this, channelId)
+        } else {
+            builder = NotificationCompat.Builder(this)
+        }
+        builder.setSmallIcon(android.R.drawable.ic_notification_overlay)
+        builder.setWhen(System.currentTimeMillis())
+        builder.setContentTitle("Content Title")
+        builder.setContentText("Content Message")
+        manager.notify(11, builder.build())
     }
 }
