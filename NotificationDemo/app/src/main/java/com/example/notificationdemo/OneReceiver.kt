21213b2c690e3ca5b4f9203527ba0537b7f82c79
hopypark <hopypark@gmail.com>
2023-08-04 10:54:11 +0900

    Create OneReceiver.kt

---
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/OneReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.notificationdemo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class OneReceiver(): BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+
+}
