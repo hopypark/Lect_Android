ae318d49024247f8ca1c8f70eeccf21d4c7819ba
hopypark <hopypark@gmail.com>
2023-08-18 11:58:19 +0900

    Create KotlinExam Project

---
+++ KotlinExam/app/src/main/java/com/example/kotlinexam/HelloKotlin.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinexam
+
+fun main(args: Array<String>){
+    println(printHello())
+    var hello:HelloKotlin = HelloKotlin()
+
+}
+
+fun printHello(): String {
+    return "Hello Kotlin World!"
+}
+
+class HelloKotlin {
+
+}
